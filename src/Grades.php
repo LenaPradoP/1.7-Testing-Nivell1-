f5a41852331493552e9ad6e20ae2af8f3a26eb86
LenaPradoP <lena@atajo.es>
2024-12-16 11:42:40 +0100

    Ejercicio 2

--- src/Grades.php
+++ src/Grades.php
@@ -3,12 +3,21 @@ declare(strict_types = 1);
 
 $Grade = 78;
 
-function calculateGrade(int $nota) : string {
+echo calculateGrade($Grade);
 
-    
+function calculateGrade(int $Grade) : string {
+    $Answer = '';
 
-        return '';
-}
-
+    if ($Grade >= 60) {
+        $Answer = 'El alumno está en Primera División.' .PHP_EOL;
+    } elseif ( $Grade >= 45) {
+        $Answer = 'El alumno está en Segunda División.' .PHP_EOL;
+    } elseif ( $Grade >= 33) {
+        $Answer = 'El alumno está en Tercera División.' .PHP_EOL;
+    } else {
+        $Answer = 'El alumno ha suspendido.' .PHP_EOL;
+    }
+        return $Answer;
+    }
 
 ?>
--- tests/GradesTest.php
+++ tests/GradesTest.php
@@ -7,45 +7,32 @@ class GradesTest extends TestCase {
 
     public function testFirstDivision()
     {
-        $this->assertEquals('Primera Divisió', calculateGrade(75));
+        $this->assertEquals('El alumno está en Primera División.' .PHP_EOL, calculateGrade(75));
     }
 
     public function testSecondDivision()
     {
-        $this->assertEquals('Segona Divisió', calculateGrade(50));
+        $this->assertEquals('El alumno está en Segunda División.' .PHP_EOL, calculateGrade(50));
     }
 
     public function testThirdDivision()
     {
-        $this->assertEquals('Tercera Divisió', calculateGrade(40));
+        $this->assertEquals('El alumno está en Tercera División.' .PHP_EOL, calculateGrade(40));
     }
 
     public function testFail()
     {
-        $this->assertEquals('Reprovat', calculateGrade(20));
+        $this->assertEquals('El alumno ha suspendido.' .PHP_EOL, calculateGrade(20));
     }
 
     public function testBoundaryValues()
     {
-        $this->assertEquals('Primera Divisió', calculateGrade(60));
-        $this->assertEquals('Segona Divisió', calculateGrade(45));
-        $this->assertEquals('Tercera Divisió', calculateGrade(33));
-        $this->assertEquals('Reprovat', calculateGrade(32));
+        $this->assertEquals('El alumno está en Primera División.' .PHP_EOL, calculateGrade(60));
+        $this->assertEquals('El alumno está en Segunda División.' .PHP_EOL, calculateGrade(45));
+        $this->assertEquals('El alumno está en Tercera División.' .PHP_EOL, calculateGrade(33));
+        $this->assertEquals('El alumno ha suspendido.' .PHP_EOL, calculateGrade(32));
     }
 
-
-function calculateGrade($percentage)
-{
-    if ($percentage >= 60) {
-        return 'Primera Divisió';
-    } elseif ($percentage >= 45) {
-        return 'Segona Divisió';
-    } elseif ($percentage >= 33) {
-        return 'Tercera Divisió';
-    } else {
-        return 'Reprovat';
-    }
 }
 
-}
 ?>
